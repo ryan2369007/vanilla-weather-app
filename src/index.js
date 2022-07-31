323c8f64e5fd1657af0198fb4049e6f68d263c35
Ryan Colon <ryan.a.colon@gmail.com>
2022-07-31 14:22:10 -0400

    add search capabilities

--- src/index.js
+++ src/index.js
@@ -54,8 +54,20 @@ function displayTemperature(response) {
   );
 }
 
-let apiKey = "a4fd6fb8e90d2a443d683786086fba14";
-let city = "Madrid";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+    let apiKey = "a4fd6fb8e90d2a443d683786086fba14";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(displayTemperature);
+    axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+
+search("New York")
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
