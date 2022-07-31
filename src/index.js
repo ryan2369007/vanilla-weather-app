b078a2085f3c2cff20c5bd4c3011be0e560e9828
Ryan Colon <ryan.a.colon@gmail.com>
2022-07-31 13:43:16 -0400

    connected current temp icon to API

--- src/index.js
+++ src/index.js
@@ -25,9 +25,6 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-    console.log(response.data);
-    console.log(response.data.dt);
-
 
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
@@ -46,10 +43,19 @@ function displayTemperature(response) {
 
   let dateElement = document.querySelector("#date");
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt",
+  `(response.data.weather[0].description)`
+  );
 }
 
 let apiKey = "a4fd6fb8e90d2a443d683786086fba14";
-let city = "Youngstown";
+let city = "Madrid";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
