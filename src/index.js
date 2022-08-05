f8c92b82afa19e9273279e376e62e49800d5ddaf
Ryan Colon <ryan.a.colon@gmail.com>
2022-08-05 18:11:38 -0400

    created an API for the forecast

--- src/index.js
+++ src/index.js
@@ -24,7 +24,8 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data);
   let forecastElement = document.querySelector("#forecast");
 
 
@@ -49,10 +50,15 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
-displayForecast();
+
+function getForecast(coordinates) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayForecast)
+}
 
 function displayTemperature(response) {
-
   let temperatureElement = document.querySelector("#temperature");
   celsiusTemp = response.data.main.temp;
   temperatureElement.innerHTML = Math.round(celsiusTemp);
@@ -80,10 +86,12 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt",
   `(response.data.weather[0].description)`
   );
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
-    let apiKey = "a4fd6fb8e90d2a443d683786086fba14";
+    let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(displayTemperature);
@@ -123,4 +131,4 @@ fahrenheitLink.addEventListener("click", showFahrenheitTemp);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", showCelsiusTemp);
 
-search("New York")
+search("Boston")
